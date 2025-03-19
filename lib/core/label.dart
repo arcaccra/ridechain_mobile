2a9d06b93dbc0dba0eaef6f16bed71f8a2fd99e2
DartVada <41478503+Developerdavido@users.noreply.github.com>
2025-03-19 23:42:37 +0000

    started working on the strings

---
+++ lib/core/label.dart
@@ -0,0 +1,66 @@
+
+
+abstract class AppConstant {
+
+  //--------------------------------------------------------------------
+  //app name
+  static const appNameLabel = "RideShare";
+
+  //--------------------------------------------------------------------
+  //splash screen
+
+  static const splashScreenOnboardingFirstLabel = "Share Ride in affordable way";
+  static const splashScreenOnboardingSecondLabel = "Its transparent & priceless";
+  static const splashScreenOnboardingThirdLabel = "Unlike other rides, you have a say in the ride";
+
+  //--------------------------------------------------------------------
+  //landing screen
+
+  static const landingScreenTitleLabel = "Your journey starts here";
+  static const landingScreenMessageLabel = "Ride with ease or drive and earn";
+
+  //--------------------------------------------------------------------
+  //buttons
+
+  static const buttonLoginLabel = "Login";
+  static const buttonGoogleLabel = "Google";
+  static const buttonVerifyLabel = "Verify";
+  static const buttonRegisterLabel = "Register";
+  static const buttonContinueLabel = "Continue";
+
+  //--------------------------------------------------------------------
+  //login screen
+
+  static const loginSignUpLabel = "Sign up";
+  static const loginPasswordHintLabel = "Password";
+  static const loginFullNameHintLabel = "Full Name";
+  static const loginEmailHintLabel = "Email Address";
+  static const loginScreenTitleLabel = "Welcome Back!";
+  static const loginPhoneNumberHintLabel = "Phone number";
+  static const loginNoAccountLabel = "You don't have an account?";
+  static const loginScreenMessageLabel = "Get back on the road-log in now!";
+
+  //--------------------------------------------------------------------
+  //register screen
+
+  static const registerSignInLabel = "Sign in";
+  static const registerScreenTitleLabel = "Join Us Today!";
+  static const registerYesAccountLabel = "Already have an account?";
+  static const registerScreenMessageLabel = "Ride, drive, or both-sign up and explore the possibilities!";
+
+  //--------------------------------------------------------------------
+  //verify account screen
+
+  static const verifyScreenInLabel = "in";
+  static const verifyScreenResentOtpLabel = "Resend OTP";
+  static const verifyChangePhoneLabel = "Change Phone Number";
+  static const verifyScreenTitleLabel = "Verify Your Account!";
+  static const verifyScreenNoOtpLabel = "Didn't receive the code?";
+  static const verifyScreenMessageLabel = "Enter the 6-digit code we sent to your phone/email to continue!";
+
+  //---------------------------------------------------------------------
+  //shared labels
+
+  static const loginContinueLabel = "or continue with";
+
+}
